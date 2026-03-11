c17de3028fe43b5d66d4f8ab42e1020fcc53ce46
RMA-MUN <3032747608@qq.com>
2026-03-11 15:47:12 +0800

    增加接口时间监测中间件

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+import time
+
+from fastapi import FastAPI, Request
 from starlette.middleware.cors import CORSMiddleware
 
 from app.router.routers import router as api_router
@@ -7,7 +9,13 @@ from app.services.session_manager import SessionManager
 
 app = FastAPI()
 
-
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(round(process_time, 4))
+    return response
 
 # 集成API路由
 app.include_router(api_router)
