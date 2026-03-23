368d52a45ba2f84e11ef5435aef52045cac1f117
RMA-MUN <3032747608@qq.com>
2026-03-23 14:36:36 +0800

    feat:增加会话管理

--- front/src/components/TabBar.vue
+++ front/src/components/TabBar.vue
@@ -1,12 +1,13 @@
 <template>
   <van-tabbar v-model="active" route>
     <van-tabbar-item to="/aichat" icon="chat-o">{{ $t('nav.aiChat') }}</van-tabbar-item>
+    <van-tabbar-item to="/sessions" icon="list">{{ $t('nav.sessions') }}</van-tabbar-item>
     <van-tabbar-item to="/my" icon="user-o">{{ $t('nav.my') }}</van-tabbar-item>
   </van-tabbar>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
@@ -17,13 +18,20 @@ const setActiveTab = () => {
   const path = route.path
   if (path.includes('/aichat')) {
     active.value = 0
-  } else if (path.includes('/my')) {
+  } else if (path.includes('/sessions')) {
     active.value = 1
+  } else if (path.includes('/my')) {
+    active.value = 2
   }
 }
 
 // 初始化时设置激活标签
 setActiveTab()
+
+// 监听路由变化，更新激活标签
+watch(() => route.path, () => {
+  setActiveTab()
+})
 </script>
 
 <style scoped>
